43b0b08b5ed7865017718a8f5ad97ea80acdbc12
fusshlach <fusshlach@gmail.com>
2023-02-03 17:41:11 +0100

    Script of movement and push
    Script of movement with obstacle and pushing block

--- Mini Jame Gam/Assets/Script/PlayerMovementScript.cs
+++ Mini Jame Gam/Assets/Script/PlayerMovementScript.cs
@@ -2,17 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private GameObject[] Obstacle;
+    private GameObject[] Movable;
+    private GameObject[] MovableWithKey;
+    private GameObject[] Hurtfull;
+
+    private bool ReadyToMove;
     void Start()
     {
-        
+        Obstacle = GameObject.FindGameObjectsWithTag("Obstacle");
+        Movable = GameObject.FindGameObjectsWithTag("Movable");
+        MovableWithKey = GameObject.FindGameObjectsWithTag("MovableWithKey");
+        Hurtfull = GameObject.FindGameObjectsWithTag("Hurtfull");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveinput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        moveinput.Normalize();
+
+        if(moveinput.sqrMagnitude > 0.5)
+        {
+            if(ReadyToMove)
+            {
+                ReadyToMove = false;
+                Move(moveinput);
+            }
+        }
+        else
+        {
+            ReadyToMove = true;
+        }
     }
+
+    public bool Move(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) < 0.5)
+
+        {
+            direction.x = 0;
+        }
+        else
+
+        {
+            direction.y = 0;
+        }
+        direction.Normalize();
+
+        if (Blocked(transform.position, direction))
+        {
+            return false;
+        }
+        else
+        {
+            transform.Translate(direction);
+            return true;
+        }
+    }
+
+    public bool Blocked(Vector3 position, Vector2 direction)
+    {
+        Vector2 newpos = new Vector2(position.x, position.y) + direction;
+
+        foreach (var obj in Obstacle)
+        {
+            if (obj.transform.position.x == newpos.x && obj.transform.position.y == newpos.y)
+            {
+                return true;
+            }
+        }
+
+        foreach (var Movable in Movable)
+        {
+            if (Movable.transform.position.x == newpos.x && Movable.transform.position.y == newpos.y)
+            {
+                Push objpush = Movable.GetComponent<Push>();
+                if (objpush && objpush.Move(direction))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
---
+++ Mini Jame Gam/Assets/Script/Push.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    private GameObject[] Obstacle;
+    private GameObject[] Movable;
+    private GameObject[] MovableWithKey;
+    private GameObject[] Hurtfull;
+  
+    void Start()
+    {
+        Obstacle = GameObject.FindGameObjectsWithTag("Obstacle");
+        Movable = GameObject.FindGameObjectsWithTag("Movable");
+        MovableWithKey = GameObject.FindGameObjectsWithTag("MovableWithKey");
+        Hurtfull = GameObject.FindGameObjectsWithTag("Hurtfull");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool Move(Vector2 direction)
+    {
+        if(ObjtoBlocked(transform.position,direction))
+        {
+            return false;
+        }
+        else
+        {
+            transform.Translate(direction);
+            return true;
+        }
+    }
+
+    public bool ObjtoBlocked(Vector3 position, Vector2 direction)
+    {
+        Vector2 newpos = new Vector2(position.x, position.y) + direction;
+
+        foreach (var obj in Obstacle)
+        {
+            if (obj.transform.position.x == newpos.x && obj.transform.position.y == newpos.y)
+            {
+                return true;
+            }
+        }
+
+        foreach (var Movable in Movable)
+        {
+            if (Movable.transform.position.x == newpos.x && Movable.transform.position.y == newpos.y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
