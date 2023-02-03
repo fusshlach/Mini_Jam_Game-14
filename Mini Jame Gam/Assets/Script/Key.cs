61dbfa58b4de70adadfd67ef1bf912fe1b98ad82
Milis <camille.lecappon@wanadoo.fr>
2023-02-03 23:55:48 +0100

    Bug Fix Key

--- Mini Jame Gam/Assets/Script/Key.cs
+++ Mini Jame Gam/Assets/Script/Key.cs
@@ -8,9 +8,12 @@ public class Key : MonoBehaviour
 {
     public bool Unlock = false;
     private GameObject[] MovableWithKey;
+    private int arraylenght;
     void Start()
     {
         MovableWithKey = GameObject.FindGameObjectsWithTag("MovableWithKey");
+        arraylenght = MovableWithKey.Length;
+        
 
 
 
@@ -21,8 +24,27 @@ public class Key : MonoBehaviour
     void OnTriggerEnter(Collider obj)
     {
         if (obj.transform.name == "Player")
+        {
             Unlock = true;
+        }
+            
+        
+        
     }
+    void Update()
+    {
+        if (arraylenght > 0 && Unlock == true)
+        {
+            for (var i = 0; i < arraylenght; i++)
+            {
+                DestroyImmediate(MovableWithKey[i]);
+               
+            }
+            DestroyImmediate(this.gameObject);
+        }
+    }
+
+
 }
   
       
