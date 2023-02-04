7b2ce18b0925d78878cef2b6b46fd80b2410dc83
Milis <camille.lecappon@wanadoo.fr>
2023-02-04 14:57:44 +0100

    TMP fix

--- Mini Jame Gam/Assets/Script/EnergyLevelScript.cs
+++ Mini Jame Gam/Assets/Script/EnergyLevelScript.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
+using TMPro;
 
 public class EnergyLevelScript : MonoBehaviour
 {
-    public InputField inputField;
-    public Text 12;
-    private int value = 0;
+   // public InputField inputField;
+    public int energy = 12;
+    [SerializeField]
+    public GameObject Text;
+    private TMP_InputField TextInput;
+    void Start()
+    {
+        TextInput = Text.GetComponent<TMP_InputField>();
+        Debug.Log(TextInput);
+        TextInput.text = energy.ToString();
+        Debug.Log(Text);
+    }
 
     public void OnValueChanged()
     {
-        value++;
-        text.text = value.ToString();
     }
 }
 
