86119842ed2900fd1b08a54efd095ab0e3a60867
Milis <camille.lecappon@wanadoo.fr>
2023-02-05 12:55:13 +0100

    timingVictory+Vague

--- Mini Jame Gam/Assets/Script/FinishScript.cs
+++ Mini Jame Gam/Assets/Script/FinishScript.cs
@@ -25,7 +25,7 @@ public class FinishScript : MonoBehaviour
         {
             Color Newcolor = new Color(1f, 1f, 1f, alph);
             mgraph.color = Newcolor;
-            alph += 0.002f;
+            alph += 0.004f;
             if (alph > 1)
             {
                 SceneManager.LoadScene(NextScene);
